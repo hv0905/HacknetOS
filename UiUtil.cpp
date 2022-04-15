87012f916948d03f74b1cc378bfca20e17576a38
EdgeNeko <edgeneko@aiursoft.com>
2022-04-15 22:18:11 +0800

    Add UiUtil

---
+++ UiUtil.cpp
@@ -0,0 +1,5 @@
+//
+// Created by epiphyllum on 22/04/15.
+//
+
+#include "UiUtil.h"
---
+++ UiUtil.h
@@ -0,0 +1,15 @@
+//
+// Created by epiphyllum on 22/04/15.
+//
+
+#ifndef HACKNETOS_UIUTIL_H
+#define HACKNETOS_UIUTIL_H
+
+
+namespace UIUtil
+{
+
+}
+
+
+#endif //HACKNETOS_UIUTIL_H
