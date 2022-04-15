96adbb740a0aa4b1c659290aa26b8e8010bf1f9a
anti-akane <1695084606@qq.com>
2022-04-15 10:53:58 +0800

    Initialize the email

--- HackEmail.cpp
+++ HackEmail.cpp
@@ -2,21 +2,22 @@
 // Created by 16950 on 2022/4/15.
 //
 #include"HackEmail.h"
-HackEmail::HackEmail(const std::string &title,const std::string &sender,const std::string &content,int mid,bool deleted):
-emailTitle(title),sender(sender),emailContent(content),missionId(mid),deleted(deleted)
-{};
-const HackEmail Email[100]={
-        HackEmail("Diana","Diana","Diana",1,0)
+
+#include <utility>
+HackEmail::HackEmail(std::string title,std::string sender,std::string content,int mid,bool deleted):
+emailTitle(std::move(title)),sender(std::move(sender)),emailContent(std::move(content)),missionId(mid),deleted(deleted){};
+const HackEmail Email[100]= {
+        HackEmail("Diana", "Diana", "Diana", 1, 0)
 };
-void HackEmail::lsMail(int missionId){
-    for(int i=0;i<100;i++)
-        if(Email[i].missionId<=missionId&&Email[i].deleted==0){
+void HackEmail::lsMail(int missionId) {
+    for (const auto & i : Email)
+        if (i.missionId <= missionId && i.deleted == 0) {
             //显示邮件
-            }
+        }
 }
 void HackEmail::cdMail(const std::string& title) {
-    for(int i=0;i<100;i++)
-        if(Email[i].emailTitle==title&&Email[i].deleted==0){
+    for (const auto & i : Email)
+        if (i.emailTitle == title && i.deleted == 0) {
             //显示邮件内容
         }
 }
--- HackEmail.h
+++ HackEmail.h
@@ -1,21 +1,23 @@
 //
 // Created by 16950 on 2022/4/15.
 //
-
 #ifndef HACKNETOS_HACKEMAIL_H
 #define HACKNETOS_HACKEMAIL_H
 #include "string"
-class HackEmail
-{
+class HackEmail {
 public:
-HackEmail(const std::string &title="",const std::string &sender="",const std::string &content="",int mid=0,bool deleted=1);
-void lsMail(int missionId);
-void cdMail(const std::string &title);
+    explicit HackEmail(std::string title = "", std::string sender = "", std::string content = "",
+              int mid = 1000, bool deleted = 1);
+
+    static void lsMail(int missionId);
+
+    static void cdMail(const std::string &title);
+
 private:
-   std::string emailTitle;
-std::string sender;
-std::string emailContent;
- int missionId;
-bool deleted;
+    std::string emailTitle;
+    std::string sender;
+    std::string emailContent;
+    int missionId;
+    bool deleted;
 };
 #endif //HACKNETOS_HACKEMAIL_H
