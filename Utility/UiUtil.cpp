b2f233a5aba31746a4cf334b8cb4711ba0ff2031
EdgeNeko <edgeneko@aiursoft.com>
2022-04-21 00:12:50 +0800

    Better UI framework

--- Utility/UiUtil.cpp
+++ Utility/UiUtil.cpp
@@ -9,43 +9,40 @@
 using namespace Util;
 using std::cout;
 
-// 220 * 50
 
-// MEM: 40 * 49 (1 line) 0 ~ 39 1 ~ 49
-
-// STATUS 179 * 5 (1 line) 41 ~ 219 1 ~ 5
-
-// TERM 179 * 43  7 ~ 49
 
 void UIUtil::drawFramework()
 {
     // Title of MEM
     setCursorPos(0, 0);
-    cout << "-<MEMORY>"; // 9
+    setColorAttr(BG_WHITE);
+    setColorAttr(FG_BLACK);
+    cout << " <MEMORY>"; // 9
     for (int i = 0; i < 31; i++)
     {
-        cout << '-';
+        cout << ' ';
     }
     cout << ' ';
     // Title of STATUS
-    cout << "-<STATUS>"; // 9
+    cout << " <STATUS>"; // 9
     for (int i = 0; i < 170; i++)
     {
-        cout << '-';
+        cout << ' ';
     }
     // Draw a vertical line for MEM | TERM x = 40 l = 50
     for (int i = 0; i < 50; ++i)
     {
         setCursorPos(40, i);
-        cout << '|';
+        cout << ' ';
     }
 
     // Draw a horizontal line for STATUS | TERM
     setCursorPos(41, 6);
-    cout << "-<TERMINAL>"; // 11
+    cout << " <TERMINAL>"; // 11
     for (int i = 0; i < 168; i++)
     {
-        cout << '-';
+        cout << ' ';
     }
+    setColorAttr(ATTR_NORMAL);
 
 }
