cf52ec9e9c6fdbb02f6c8097023b94961e8e50ac
EdgeNeko <edgeneko@aiursoft.com>
2022-04-25 20:40:20 +0800

    Add executive command autocomplete

--- HacknetApplication.cpp
+++ HacknetApplication.cpp
@@ -695,23 +695,31 @@ RenderService &HacknetApplication::getRenderService()
 
 std::string HacknetApplication::getCommandAutoComplete(const std::string &command)
 {
-    auto cmdLower = StringUtil::toLowerCopy(command);
-    std::vector<std::string> possibleResult;
-    for (const auto &i: globalCommands)
-    {
+auto cmdLower = StringUtil::toLowerCopy(command);
+        std::vector<std::string> possibleResult;
+        for (const auto &i: globalCommands)
+        {
         if (StringUtil::toLowerCopy(i.getPrefix()).rfind(cmdLower, 0) == 0)
         {
-            possibleResult.emplace_back(i.getPrefix());
+        possibleResult.emplace_back(i.getPrefix());
         }
-    }
-    for (int i = 1; i < possibleResult.size(); i++)
-    {
+        }
+
+        for (const auto &i : getAvailExecutiveCommand()) {
+        if (StringUtil::toLowerCopy(i.getPrefix()).rfind(cmdLower, 0) == 0)
+        {
+        possibleResult.emplace_back(i.getPrefix());
+        }
+        }
+
+        for (int i = 1; i < possibleResult.size(); i++)
+        {
         pushLog(possibleResult[i]);
         renderService.requireUpdate = true;
-    }
-    if (possibleResult.empty())
+        }
+        if (possibleResult.empty())
         return command;
-    else
+        else
         return StringUtil::getPublicPrefix(possibleResult);
 }
 
